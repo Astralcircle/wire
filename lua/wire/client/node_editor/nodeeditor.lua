0c66b9c7516689bec62ae06e192ee7339df5afc9
Lysdal <lysdal1234@gmail.com>
2026-02-27 10:50:27 +0100

    Added descriptions to some FPGA and CPU gates, along with tooltips in the gate selection menu

--- lua/wire/client/node_editor/nodeeditor.lua
+++ lua/wire/client/node_editor/nodeeditor.lua
@@ -452,6 +452,7 @@ function Editor:InitComponents()
 			local action, gate = subtree[index].action, subtree[index].gate
 			local node2 = node:AddNode(gate.name or "No name found :(")
 			node2.name = gate.name
+			node2:SetTooltip(gate.description)
 			node2.action = action
 			function node2:DoClick()
 				editor.SelectedInMenu = { type = type, gate = self.action }
